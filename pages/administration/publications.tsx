686c9df00352a64385824c9953edd3814c28cb0f
Noe-Louvell <noe.louvel@linkt.fr>
2022-07-07 17:40:27 +0200

    Fix CI 1.0.7

--- pages/administration/publications.tsx
+++ pages/administration/publications.tsx
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import * as React from 'react';
 import { TableRessource } from '../../Components/Administration/Publications/TableRessource';
 import Page from '../../Components/generic/Page/Page';
 import { getAllRessources } from '../../services/ressource.service';
 
-export default function AdminPublications({ publicationData = [] }) {
+export default function AdminPublications({ publicationData }) {
     return (
         <Page
             title='Admin Publication'
@@ -14,9 +15,12 @@ export default function AdminPublications({ publicationData = [] }) {
     );
 }
 
-export async function getStaticProps({ params }) {
-    const res = await getAllRessources();
-    const publicationData = await res.data;
+export async function getStaticProps() {
+    const response = await axios({
+        method: 'get',
+        url: 'http://localhost:3000/api/ressource',
+    })
+    const publicationData = await response.data;
     return {
         props: {
             publicationData
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,13 @@ import { getAllRessources } from '../services/ressource.service';
 import { ContextApp } from '../Context/ContextAuth/ContextAuth';
 
 export default function Index({ resRessources = [] }) {
-    const { userSession } = useContext(ContextApp);
+    // const { userSession } = useContext(ContextApp);
     return (
         <>
             <Page
                 title='Accueil'
             >
                 <ListRessources ressources={resRessources} />
-                <FormulaireRessource disable={Object.keys(userSession).length !== 0 ? false : false} type='create' />
             </Page>
         </>
 
